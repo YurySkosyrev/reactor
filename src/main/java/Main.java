bb3b7c0fbf31123b2ad655e528c3a9028b11e6e4
Aviaurez <skosyrev-87@mail.ru>
2023-01-11 07:15:39 +0300

    Project reactor example part 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -60,20 +60,57 @@ public class Main {
 
 
         Flux // есть аналог push - только он однопоточный
-                .create(sink ->
-                        telegramProducer.subscribe(new BaseSubscriber<Object>() {
-                            @Override
-                            protected void hookOnNext(Object value) {
-                                sink.next(value);
-                            }
+             // Produser активный и сам через subscription скидывает сообщения
+             // Ex massenger очередей, либо внешние hookи прилетают
 
-                            @Override
-                            protected void hookOnComplete() {
-                                sink.complete();
-                            }
-                        })
-                )
-                .subscribe(System.out::println);
+                .create(sink -> {
+                    telegramProducer.subscribe(new BaseSubscriber<Object>() {
+                        @Override
+                        protected void hookOnNext(Object value) {
+                            sink.next(value);
+                        }
+
+                        @Override
+                        protected void hookOnComplete() {
+                            sink.complete();
+                        }
+                    });
+
+                    // Здесь делаем pull из внешнего сервиса
+                    sink.onRequest(r -> {
+                        sink.next("DB returns:" + telegramProducer.blockFirst());
+                    });
+                });
+
+        Flux<String> second = Flux
+                .just("World", "Coder")
+                .repeat();
+
+        Flux<String> sumFlux = Flux
+                .just("hello", "dru", "java", "Linus", "Asia")
+                .zipWith(second, (f, s) -> String.format("%s %s", f, s));
+
+        Flux<String> stringFlux = sumFlux
+                .delayElements(Duration.ofMillis(1300))
+                .timeout(Duration.ofSeconds(1)) // обрываем через 1 секунду и вываливается ошибка
+                .retry(3) // попробовать 3 раза
+//                .onErrorReturn("Too slow") // выводим сообщение вместо ошибки
+                .onErrorResume(throwable ->
+                        Flux
+                                .interval(Duration.ofMillis(300))
+                                .map(String::valueOf))
+                .skip(2)
+                .take(4);
+
+        stringFlux.subscribe(
+                v -> System.out.println(v),
+
+                e -> System.err.println(e),
+                () -> System.out.println("finished")
+        );
+        Thread.sleep(5000l);
+
+//        stringFlux.toIterable(); можно преобразовать к любому типу
 
     }
 }
